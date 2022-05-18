3700ebdb6660ea0625376990fc612d4128410698
Niklas Johansson <niklasdjohansson@gmail.com>
2022-05-18 11:06:08 +0200

    update

--- installable-example/service-worker.js
+++ installable-example/service-worker.js
@@ -1,3 +1,32 @@
-self.addEventListener('fetch', () => {
- // Vi återkommer till detta nästa vecka
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll(['offline.html', 'styles.css']);
+        })
+    )
+    console.log('Installed service worker at ', new Date().toLocaleTimeString());
+});
+
+self.addEventListener('activate', (event) => {
+    self.skipWaiting();
+    console.log('Activated service worker at ', new Date().toLocaleTimeString());
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(event.request.url);
+    if(!navigator.onLine){
+        console.log("du är offline");
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                console.log(response)
+                if(response){
+                    return response
+                } else {
+                    return caches.match(new Request('offline.html'));
+                }
+            })
+        );
+    }else {
+        console.log("du är online")
+    }
 });
